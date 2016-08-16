f862501c87919a1c349303c1f62bb012a4f4223b
Bruno Barreto <bruno@brunobarreto.net>
2016-08-16 15:51:19 +0100

    Filtro de data na home

--- html/index.php
+++ html/index.php
@@ -13,6 +13,18 @@ $data_minima = $db->search([
     ]
 ]);
 
+if (isset($_GET['inicio']) && count(explode("/", $_GET['inicio']))==3):
+    $inicio = strtotime($_GET['inicio'].' 00:00:00');
+else:
+    $inicio = strtotime($data_minima['hits']['hits'][0]['_source']['timestamp']);
+endif;
+
+if (isset($_GET['fim']) && count(explode("/", $_GET['fim']))==3):
+    $fim = strtotime($_GET['fim'].' 00:00:00');
+else:
+    $fim = time();
+endif;
+
 $agregacoes = $db->search([
     'index' => 'solicitacoes',
     'type' => 'solicitacao',
@@ -22,13 +34,27 @@ $agregacoes = $db->search([
     		[ "timestamp"=>"desc" ]
     	],
 		"aggs"=> [
-			"subprefeitura"=>["terms"=>["field"=>"subprefeitura.raw","size"=>10]],
-			"canal"=>["terms"=>["field"=>"canal.raw","size"=>10]],
-			"assunto"=>["terms"=>["field"=>"assunto.raw","size"=>10]],
-			"orgao"=>["terms"=>["field"=>"orgao.raw","size"=>10]]
+
+            "intervalo"=> [
+                "date_range"=> [
+                    "field"=> "timestamp",
+                    "format"=> "MM-yyy",
+                    "ranges"=> [
+                        [ "from"=> $inicio*1000, "to"=> $fim*1000 ] 
+                    ]
+                ],
+                "aggs"=>[
+                    "subprefeitura"=>["terms"=>["field"=>"subprefeitura.raw","size"=>10]],
+                    "canal"=>["terms"=>["field"=>"canal.raw","size"=>10]],
+                    "assunto"=>["terms"=>["field"=>"assunto.raw","size"=>10]],
+                    "orgao"=>["terms"=>["field"=>"orgao.raw","size"=>10]]
+                ]
+            ]
+
 		],
 
     ]
 ]);
+
 
 include("template/home.phtml");
